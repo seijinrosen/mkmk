baaeb70371b814457fdfb10c94877c4d1b58fad4
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-04 23:52:31 +0900

    エラー処理を追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,8 +13,18 @@ fn main() {
         }
 
         let path = Path::new(arg);
-        let parent = path.parent().unwrap();
-        fs::create_dir_all(parent).unwrap();
-        File::create(path).unwrap();
+        let parent = path.parent();
+
+        if let Some(parent) = parent {
+            fs::create_dir_all(parent).unwrap_or_else(|err| {
+                println!("エラー発生: {}", err);
+                process::exit(1);
+            });
+        }
+
+        File::create(path).unwrap_or_else(|err| {
+            println!("エラー発生: {}", err);
+            process::exit(1);
+        });
     }
 }

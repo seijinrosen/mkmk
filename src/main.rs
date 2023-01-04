d2786bfee2650230df854128561bfc7b1b2bb7e4
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-05 03:41:24 +0900

    for は main 関数に残す

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
@@ -12,26 +13,27 @@ fn main() {
         process::exit(0);
     }
 
-    run(args);
-}
-
-fn run(args: Vec<String>) {
     for (i, arg) in args.iter().enumerate() {
         if i == 0 {
             continue;
         }
 
-        let path = Path::new(arg);
-
-        if let Some(parent) = path.parent() {
-            if let Err(e) = fs::create_dir_all(parent) {
-                println!("mkdir error: {:?} {}", parent, e);
-                continue;
-            }
-        }
-
-        if let Err(e) = File::create(path) {
-            println!("touch error: {:?} {}", path, e);
+        if let Err(e) = run(arg) {
+            println!("error: {}: {}", arg, e);
         }
     }
+}
+
+fn run(arg: &String) -> Result<(), Box<dyn Error>> {
+    let path = Path::new(arg);
+
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent)?;
+        // println!("mkdir error: {:?} {}", parent, e);
+    }
+
+    File::create(path)?;
+    // println!("touch error: {:?} {}", path, e);
+
+    Ok(())
 }
